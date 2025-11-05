d49b729172de4366b615ed6f8a7da4def0ad926b
Shomalik Davlatov <shomalikdavlatov@gmail.com>
2025-11-05 18:21:14 +0500

    new firebase added

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAKsnULuwYs2avJ7weNIr2I6ErnhZRahak",
-  authDomain: "abc-tezdev.firebaseapp.com",
-  projectId: "abc-tezdev",
-  storageBucket: "abc-tezdev.firebasestorage.app",
-  messagingSenderId: "810569066209",
-  appId: "1:810569066209:web:9e5aa228ee4068f680f9a0",
-  measurementId: "G-7KF81DG606"
+    apiKey: "AIzaSyBe22t6Jdea-h2WulOj5hOc87TH-ZkG5p0",
+    authDomain: "abc-tezdev-f5631.firebaseapp.com",
+    projectId: "abc-tezdev-f5631",
+    storageBucket: "abc-tezdev-f5631.firebasestorage.app",
+    messagingSenderId: "892841549169",
+    appId: "1:892841549169:web:66b2b7d8719ec07ccb6ad0",
+    measurementId: "G-L0CHKRWC2W",
 };
 
 const app = initializeApp(firebaseConfig);
